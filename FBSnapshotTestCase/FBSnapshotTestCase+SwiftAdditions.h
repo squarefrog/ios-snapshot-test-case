4e587ad01e6c3dcfc79b003bc3a23d6e70d33d09
Paul Williamson <squarefrog@gmail.com>
2015-04-19 19:00:49 +0100

    Add documentation

--- FBSnapshotTestCase/FBSnapshotTestCase+SwiftAdditions.h
+++ FBSnapshotTestCase/FBSnapshotTestCase+SwiftAdditions.h
@@ -12,16 +12,40 @@
 
 @interface FBSnapshotTestCase (SwiftAdditions)
 
+/**
+ Performs a comparison test for a supplied view
+ @param view       The view to snapshot
+ @param identifier An optional identifier, used if there are multiple snapshot tests in a given -test method.
+ */
 - (void)snapshotVerifyView:(UIView *)view
             withIdentifier:(NSString *)identifier;
 
+/**
+ Performs a comparison test for a supplied view with an identifier and reference image directory path suffix
+ 
+ @param view                 The view to snapshot
+ @param identifier           An optional identifier, used if there are multiple snapshot tests in a given -test method.
+ @param imageDirectorySuffix An optional suffix, appended to the reference image directory path, such as "_iOS8"
+ */
 - (void)snapshotVerifyView:(UIView *)view
             withIdentifier:(NSString *)identifier
       imageDirectorySuffix:(NSString *)imageDirectorySuffix;
 
+/**
+ Performs a comparison test for a supplied layer
+ @param layer       The layer to snapshot
+ @param identifier An optional identifier, used if there are multiple snapshot tests in a given -test method.
+ */
 - (void)snapshotVerifyLayer:(CALayer *)layer
             withIdentifier:(NSString *)identifier;
 
+/**
+ Performs a comparison test for a supplied layer with an identifier and reference image directory path suffix
+ 
+ @param layer                 The layer to snapshot
+ @param identifier           An optional identifier, used if there are multiple snapshot tests in a given -test method.
+ @param imageDirectorySuffix An optional suffix, appended to the reference image directory path, such as "_iOS8"
+ */
 - (void)snapshotVerifyLayer:(CALayer *)layer
             withIdentifier:(NSString *)identifier
       imageDirectorySuffix:(NSString *)imageDirectorySuffix;
